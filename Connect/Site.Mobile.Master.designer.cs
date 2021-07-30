8cf0c1333e90705e4a914bea5e9eb4cab124b99e
victor-singha <vicunite2@gmail.com>
2021-07-30 12:40:17 +0530

    [victor] refactor site.mobile

--- Connect/Site.Mobile.Master.designer.cs
+++ Connect/Site.Mobile.Master.designer.cs
@@ -7,11 +7,13 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Connect {
-    
-    
-    public partial class Site_Mobile {
-        
+namespace Connect
+{
+
+
+    public partial class Site_Mobile
+    {
+
         /// <summary>
         /// HeadContent control.
         /// </summary>
@@ -20,16 +22,7 @@ namespace Connect {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
-        
-        /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
+
         /// <summary>
         /// FeaturedContent control.
         /// </summary>
@@ -38,7 +31,7 @@ namespace Connect {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder FeaturedContent;
-        
+
         /// <summary>
         /// MainContent control.
         /// </summary>

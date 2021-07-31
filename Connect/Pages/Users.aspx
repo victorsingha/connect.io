70b9c701f1532d2dce3640cde7fe98cc9bc493f6
victor-singha <vicunite2@gmail.com>
2021-07-31 15:51:58 +0530

    [victor] gridview pages/users

---
+++ Connect/Pages/Users.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Users.aspx.cs" Inherits="Connect.Pages.Users" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <form runat="server" class="container">
+        <asp:GridView ID="UsersGrid" runat="server"></asp:GridView>
+    </form>
+</asp:Content>
---
+++ Connect/Pages/Users.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Connect.Pages
+{
+    public partial class Users : System.Web.UI.Page
+    {
+        static string connectionString = ConfigurationManager.ConnectionStrings["ConnectConnectionString"].ConnectionString;
+        SqlConnection connection = new SqlConnection(connectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GetAllUsers();
+        }
+        protected void GetAllUsers()
+        {
+            try
+            {
+                using (connection)
+                {
+                    connection.Open();
+                    string query = $"SELECT * FROM Users";
+                    SqlCommand cmd = new SqlCommand(query, connection);
+                    // cmd = new SqlCommand("GetAllBookList", connection);
+                    //cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                    DataTable dt = new DataTable();
+                    //DataSet ds = new DataSet();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(dt);
+                    //BooksRepeater.DataSource = dt;
+                    //BooksRepeater.DataBind();
+
+                    UsersGrid.DataSource = dt;
+                    UsersGrid.DataBind();
+
+                    connection.Close();
+
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
